c4cd6823d550b95110408a109cdf7e1327beb1c6
Lili Qing <lili.lily.qing@gmail.com>
2023-02-11 21:43:58 +0800

    a5 tuiter 3.8

---
+++ public/tuiter/explore/PostSummaryList/index.js
@@ -0,0 +1,26 @@
+import PostSummaryItem from "./PostSummaryItem.js";
+import posts from "./posts.js";
+
+const index = () => {
+    return (`
+      <ul class="list-group">
+         ${
+        posts.map(post => {
+            return (PostSummaryItem(post));
+        }).join('')
+    }
+      </ul>
+   `);
+}
+export default index;
+
+function testPostSummaryListComponent() {
+    $('#wd-postSummaryList').append(
+        `
+        ${index()}
+    </ul>
+    `
+    );
+}
+
+$(testPostSummaryListComponent);
