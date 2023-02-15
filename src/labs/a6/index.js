2a28e779558cc810f92fdcbc96c8b5ee12accb0f
Lili Qing <lili.lily.qing@gmail.com>
2023-02-15 16:07:18 +0800

    a6 lab complete 2.10

--- src/labs/a6/index.js
+++ src/labs/a6/index.js
@@ -1,10 +1,12 @@
 import {Link}
     from "react-router-dom";
+import Classes from "./classes";
 
 function Assignment6() {
     return (
         <div>
             <h1>Assignment 6</h1>
+            <Classes/>
         </div>
     );
 }
