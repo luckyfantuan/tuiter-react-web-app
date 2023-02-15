c1c3a1cd53f9ed154388fa15765b07e44b75b66a
Lili Qing <lili.lily.qing@gmail.com>
2023-02-15 19:57:56 +0800

    a6 tuiter complete 3.3

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -2,24 +2,14 @@ import Nav from "../nav";
 import NavigationSidebar
     from "./navigation-sidebar";
 import WhoToFollowListItem from "./who-to-follow-list/who-to-follow-list-item";
+import WhoToFollowList from "./who-to-follow-list";
 
 function Tuiter() {
     return (
         <div>
             <Nav/>
             <NavigationSidebar active="home"/>
-            <WhoToFollowListItem who={{
-                userName: 'NASA', handle: 'NASA',
-                avatarIcon: 'nasa.png',
-            }}/>
-            <WhoToFollowListItem who={{
-                userName: 'Tesla', handle: 'tesla',
-                avatarIcon: 'tesla.png',
-            }}/>
-            <WhoToFollowListItem who={{
-                userName: 'SpaceX', handle: 'SpaceX',
-                avatarIcon: 'spacex.png',
-            }}/>
+            <WhoToFollowList/>
 
             <h1>Tuiter</h1>
         </div>
