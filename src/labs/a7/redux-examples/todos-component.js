c163a71f09c98d8efe5844ecd9a55e93c65bdf23
Lili Qing <lili.lily.qing@gmail.com>
2023-03-14 10:17:20 -0700

    a7 complete 2.7

--- src/labs/a7/redux-examples/todos-component.js
+++ src/labs/a7/redux-examples/todos-component.js
@@ -1,10 +1,15 @@
 import React, {useState} from "react";
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
+import {addTodo} from "./reducers/todos-reducer";
 
 const Todos = () => {
     const todos
         = useSelector(state => state.todos);
     const [todo, setTodo] = useState({do: ''});
+    const dispatch = useDispatch();
+    const createTodoClickHandler = () => {
+        dispatch(addTodo(todo))
+    }
     const todoChangeHandler = (event) => {
         const doValue = event.target.value;
         const newTodo = {
@@ -17,7 +22,8 @@ const Todos = () => {
             <h3>Todos</h3>
             <ul className="list-group">
                 <li className="list-group-item">
-                    <input onChange={todoChangeHandler} value={todo.do} className="form-control"/>
+                    <button onClick={createTodoClickHandler} className="btn btn-primary w-25 float-end">Create</button>
+                    <input onChange={todoChangeHandler} value={todo.do} className="form-control w-75"/>
                 </li>
                 {
                     todos.map(todo =>
