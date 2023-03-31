8790e97d9b9830ae1e7c01c959f579c579866445
Lili Qing <lili.lily.qing@gmail.com>
2023-03-31 15:20:31 -0700

    a9 update nav link

--- src/nav.js
+++ src/nav.js
@@ -5,7 +5,7 @@ function Nav() {
         <div>
             {/*<Link to="/">Labs</Link> |*/}
             {/*<Link to="/">Assignment 6</Link> |*/}
-            <Link to="/a7">Assignment 7</Link> |
+            {/*<Link to="/a7">Assignment 7</Link> |*/}
             <Link to="/tuiter">Tuiter</Link>
         </div>
     )
